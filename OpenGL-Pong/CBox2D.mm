cd52faa38061c3ae9c1c18932f946aad74260e9a
Mohammed Bajaman <mohammed_bajaman@hotmail.com>
2022-04-05 22:38:15 -0700

    Added comments and renamed some variables

--- OpenGL-Pong/CBox2D.mm
+++ OpenGL-Pong/CBox2D.mm
@@ -17,13 +17,11 @@ const int NUM_POS_ITERATIONS = 3;
 #pragma mark - Box2D contact listener class
 
 // This C++ class is used to handle collisions
-class CContactListener : public b2ContactListener
-{
+class CContactListener : public b2ContactListener {
 public:
     void BeginContact(b2Contact* contact) {};
     void EndContact(b2Contact* contact) {};
-    void PreSolve(b2Contact* contact, const b2Manifold* oldManifold)
-    {
+    void PreSolve(b2Contact* contact, const b2Manifold* oldManifold) {
         b2WorldManifold worldManifold;
         contact->GetWorldManifold(&worldManifold);
         b2PointState state1[2], state2[2];
@@ -42,26 +40,25 @@ public:
 
 #pragma mark - CBox2D
 
-@interface CBox2D ()
-{
-    // Box2D-specific objects
+@interface CBox2D () {
     b2Vec2 *gravity;
     b2World *world;
     b2BodyDef *groundBodyDef;
     b2Body *groundBody;
     b2PolygonShape *groundBox;
-    b2Body *Paddle1, *Paddle2, *theBall;
+    b2Body *Paddle1, *Paddle2, *theBall; // Paddles and ball
     b2Body *EWall, *WWall;
     CContactListener *contactListener;
     float totalElapsedTime;
     
-    float Paddle1_POS_X, Paddle1_POS_Y;
-    float Paddle2_POS_X, Paddle2_POS_Y;
+    float Paddle1_POS_X, Paddle1_POS_Y; // Paddle 1 position
+    float Paddle2_POS_X, Paddle2_POS_Y; // Paddle 2 position
 
-    // You will also need some extra variables here for the logic
-    bool ballHitBrick;
+    // Logic
+    bool ballHitPaddle;
     bool ballLaunched;
     
+    //Scores
     int Player1Score, Player2Score;
 }
 @end
@@ -71,51 +68,44 @@ public:
 @synthesize Paddle1_POS_X;
 @synthesize Paddle2_POS_X;
 
-- (instancetype)init //This is replacement for Hello World
-{
+- (instancetype) init {
     self = [super init];
+    
     if (self) {
         // Initialize Box2D
         gravity = new b2Vec2(0.0f, 0.0f);
         world = new b2World(*gravity);
         
-        // For HelloWorld
-        groundBodyDef = NULL;
-        groundBody = NULL;
-        groundBox = NULL;
+        //Set up initial scores
+        Player1Score = 0;
+        Player2Score = 0;
 
-        // For brick & ball sample
         contactListener = new CContactListener();
         world->SetContactListener(contactListener);
         
-        // Set up the brick and ball objects for Box2D
-        b2BodyDef Paddle1BodyDef; //Brick Definition
+        // Paddle 1 setup
+        b2BodyDef Paddle1BodyDef;
         Paddle1BodyDef.type = b2_dynamicBody;
         Paddle1_POS_X = 400.0f;
         Paddle1_POS_Y = 500.0f;
         Paddle1BodyDef.position.Set(Paddle1_POS_X, Paddle1_POS_Y);
         Paddle1 = world->CreateBody(&Paddle1BodyDef);
         
-        //Set up initial scores
-        Player1Score = 0;
-        Player2Score = 0;
-        
-        
-        // Set up the brick and ball objects for Box2D
-        b2BodyDef Paddle2BodyDef; //Brick Definition
+        // Paddle 2 setup
+        b2BodyDef Paddle2BodyDef;
         Paddle2BodyDef.type = b2_dynamicBody;
         Paddle2_POS_X = 400.0f;
         Paddle2_POS_Y = 100.0f;
         Paddle2BodyDef.position.Set(Paddle2_POS_X, Paddle2_POS_Y);
         Paddle2 = world->CreateBody(&Paddle2BodyDef);
         
-        // Set up the east wall
+        // East wall setup
         b2BodyDef EWallBodyDef;
         EWallBodyDef.type = b2_dynamicBody;
         EWallBodyDef.position.Set(EWALL_POS_X, EWALL_POS_Y);
         EWall = world->CreateBody(&EWallBodyDef);
         
-        // Set up the west wall
+        // West wall setup
         b2BodyDef WWallBodyDef;
         WWallBodyDef.type = b2_dynamicBody;
         WWallBodyDef.position.Set(WWALL_POS_X, WWALL_POS_Y);
@@ -201,69 +191,44 @@ public:
         
         //Initialize more variables
         totalElapsedTime = 0;
-        ballHitBrick = false;
+        ballHitPaddle = false;
         ballLaunched = false;
     }
     return self;
 }
 
-- (void)dealloc
-{
+- (void)dealloc {
     if (gravity) delete gravity;
     if (world) delete world;
-    if (groundBodyDef) delete groundBodyDef;
-    if (groundBox) delete groundBox;
     if (contactListener) delete contactListener;
 }
 
--(void)Update:(float)elapsedTime
-{
-    theBall->SetActive(true); //makes it part of the simulation otherwise its just sitting there
+-(void)Update:(float)elapsedTime {
+    //Enable all objects
+    Paddle1->SetAwake(true);
+    Paddle2->SetAwake(true);
+    theBall->SetActive(true);
     EWall->SetActive(true);
     WWall->SetActive(true);
     
-    // Check here if we need to launch the ball
-    //  and if so, use ApplyLinearImpulse() and SetActive(true)
+    // Launch ball in random direction with random impulse
     if(ballLaunched) {
         float r = arc4random_uniform(BALL_VELOCITY);
         float d = arc4random_uniform(2);
-        printf("\nDirection: %f\n", d);
-        printf("Impulse: %f", r);
         if(d){
             theBall->ApplyLinearImpulse(b2Vec2(r, BALL_VELOCITY), theBall->GetPosition(), true);
         } else {
             theBall->ApplyLinearImpulse(b2Vec2(-r, -100000.0f), theBall->GetPosition(), true);
         }
-        
         theBall->SetActive(true);
-#ifdef LOG_TO_CONSOLE
-        NSLog(@"Applying impulse %f to ball\n", BALL_VELOCITY);
-#endif
         ballLaunched = false;
     }
     
-    // Check if it is time yet to drop the brick, and if so
-    //  call SetAwake()
-    totalElapsedTime += elapsedTime;
-    if((totalElapsedTime > BRICK_WAIT) && Paddle1 && Paddle2){
-        Paddle1->SetAwake(true);
-        Paddle2->SetAwake(true);
-        
-        //theBall->ApplyForce(b2Vec2(10, -10), theBall->GetPosition(), true);
-    }
-    
-    // If the last collision test was positive,
-    //  stop the ball and destroy the brick
-    if(ballHitBrick){
-        //theBall->SetLinearVelocity(b2Vec2(0, 0));
-        //theBall->SetAngularVelocity(0);
-        //theBall->SetActive(false);
-        printf("BALL HIT BRICK");
+    // Apply random impulse when ball hits paddle or walls
+    if(ballHitPaddle){
         float r = arc4random_uniform(BALL_VELOCITY);
         theBall->ApplyLinearImpulse(b2Vec2(r, BALL_VELOCITY), theBall->GetPosition(), true);
-        //world->DestroyBody(theBrick);
-        //theBrick = NULL;
-        ballHitBrick = false;
+        ballHitPaddle = false;
     }
 
     if (world)
@@ -273,6 +238,7 @@ public:
             world->Step(MAX_TIMESTEP, NUM_VEL_ITERATIONS, NUM_POS_ITERATIONS);
             elapsedTime -= MAX_TIMESTEP;
             
+            // Set bounds for paddle movement
             if(Paddle2_POS_X < 675 && Paddle2_POS_X > 125) {
                 Paddle2->SetTransform(b2Vec2(Paddle2_POS_X, Paddle2_POS_Y), 0);
             }
@@ -280,6 +246,7 @@ public:
                 Paddle1->SetTransform(b2Vec2(Paddle1_POS_X, Paddle1_POS_Y), 0);
             }
             
+            // Reset the ball when Players score and increase the score
             if(theBall->GetPosition().y < 0){
                 Player1Score++;
                 theBall->SetTransform(b2Vec2(400, 300), 0);
@@ -305,12 +272,10 @@ public:
 }
 
 -(void)RegisterHit {
-    // Set some flag here for processing later...
-    ballHitBrick = true;
+    ballHitPaddle = true;
 }
 
 -(void)LaunchBall {
-    // Set some flag here for processing later...
     ballLaunched = true;
 }
 
@@ -330,67 +295,11 @@ public:
 }
 
 -(int) GetPlayer1Score {
-    //char player1ScoreChar = Player1Score + '0';
     return Player1Score;
 }
 
 -(int) GetPlayer2Score {
     return Player2Score;
-}
-
--(void)HelloWorld //This is a test where you setup the world and generate numbers. A dynamic box is falling down here.
-{
-    groundBodyDef = new b2BodyDef;
-    groundBodyDef->position.Set(0.0f, -10.0f);
-    groundBody = world->CreateBody(groundBodyDef);
-    groundBox = new b2PolygonShape;
-    groundBox->SetAsBox(50.0f, 10.0f);
-    
-    groundBody->CreateFixture(groundBox, 0.0f);
-    
-    // Define the dynamic body. We set its position and call the body factory.
-    b2BodyDef bodyDef;
-    bodyDef.type = b2_dynamicBody;
-    bodyDef.position.Set(0.0f, 4.0f);
-    b2Body* body = world->CreateBody(&bodyDef);
-    
-    // Define another box shape for our dynamic body.
-    b2PolygonShape dynamicBox;
-    dynamicBox.SetAsBox(1.0f, 1.0f);
-    
-    // Define the dynamic body fixture.
-    b2FixtureDef fixtureDef;
-    fixtureDef.shape = &dynamicBox;
-    
-    // Set the box density to be non-zero, so it will be dynamic.
-    fixtureDef.density = 1.0f;
-    
-    // Override the default friction.
-    fixtureDef.friction = 0.3f;
-    
-    // Add the shape to the body.
-    body->CreateFixture(&fixtureDef);
-    
-    // Prepare for simulation. Typically we use a time step of 1/60 of a
-    // second (60Hz) and 10 iterations. This provides a high quality simulation
-    // in most game scenarios.
-    float32 timeStep = 1.0f / 60.0f;
-    int32 velocityIterations = 6;
-    int32 positionIterations = 2;
-    
-    // This is our little game loop.
-    for (int32 i = 0; i < 60; ++i)
-    {
-        // Instruct the world to perform a single step of simulation.
-        // It is generally best to keep the time step and iterations fixed.
-        world->Step(timeStep, velocityIterations, positionIterations);
-        
-        // Now print the position and angle of the body.
-        b2Vec2 position = body->GetPosition();
-        float32 angle = body->GetAngle();
-        
-        printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
-    }
 }
 
 @end
--- OpenGL-Pong/GLESRenderer.cpp
+++ OpenGL-Pong/GLESRenderer.cpp
@@ -9,8 +9,7 @@
 #include <iostream>
 #include "GLESRenderer.hpp"
 
-static GLfloat textCanvasData[] =
-{
+static GLfloat textCanvasData[] = {
     50.0f, 525.0f, 0.0f,      0.0f, 0.0f,
     750.0f, 550.0f, 0.0f,        1.0f, 1.0f,
     50.0f, 550.0f, 0.0f,       0.0f, 1.0f,
@@ -20,6 +19,7 @@ static GLfloat textCanvasData[] =
 };
 static int textCanvasNumVert = 6;
 
+// Load shader files into memory
 char *GLESRenderer::LoadShaderFile(const char *shaderFileName) {
     FILE *fp = fopen(shaderFileName, "rb");
     if (fp == NULL)
@@ -44,6 +44,7 @@ char *GLESRenderer::LoadShaderFile(const char *shaderFileName) {
     return buf;
 }
 
+// Compile shaders
 GLuint GLESRenderer::LoadShader(GLenum type, const char *shaderSrc) {
     GLuint shader = glCreateShader(type);
     if (shader == 0)
@@ -73,6 +74,7 @@ GLuint GLESRenderer::LoadShader(GLenum type, const char *shaderSrc) {
     return shader;
 }
 
+// Load OpenGL Program by linking compiled shaders into OpenGL
 GLuint GLESRenderer::LoadProgram(const char *vertShaderSrc, const char *fragShaderSrc) {
     GLuint vertexShader = LoadShader(GL_VERTEX_SHADER, vertShaderSrc);
     if (vertexShader == 0)
@@ -121,193 +123,11 @@ GLuint GLESRenderer::LoadProgram(const char *vertShaderSrc, const char *fragShad
     return programObject;
 }
 
-int GLESRenderer::GenSquare(float scale, float **vertices, int **indices) {
-    int i;
-    int numVertices = 4;
-    int numIndices = 6;
-    
-    float cubeVerts[] =
-    {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, 0.5f,  0.0f,
-        -0.5f, 0.5f,  0.0f,
-        0.5f, -0.5f,  0.0f,
-    };
-    
-    // Allocate memory for buffers
-    if ( vertices != NULL )
-    {
-        *vertices = (float *)malloc ( sizeof ( float ) * 3 * numVertices );
-        memcpy ( *vertices, cubeVerts, sizeof ( cubeVerts ) );
-        
-        for ( i = 0; i < numVertices * 3; i++ )
-        {
-            ( *vertices ) [i] *= scale;
-        }
-    }
-    
-    // Generate the indices
-    if ( indices != NULL )
-    {
-        GLuint cubeIndices[] =
-        {
-            0, 1, 2,
-            0, 3, 1,
-        };
-        
-        *indices = (int *)malloc ( sizeof ( int ) * numIndices );
-        memcpy ( *indices, cubeIndices, sizeof ( cubeIndices ) );
-    }
-    
-    return numIndices;
-}
-
-
-int GLESRenderer::GenCube(float scale, float **vertices, float **normals, float **texCoords, int **indices) {
-int i;
-int numVertices = 24;
-int numIndices = 36;
-
-float cubeVerts[] =
-{
-    -0.5f, -0.5f, -0.5f,
-    -0.5f, -0.5f,  0.5f,
-    0.5f, -0.5f,  0.5f,
-    0.5f, -0.5f, -0.5f,
-    -0.5f,  0.5f, -0.5f,
-    -0.5f,  0.5f,  0.5f,
-    0.5f,  0.5f,  0.5f,
-    0.5f,  0.5f, -0.5f,
-    -0.5f, -0.5f, -0.5f,
-    -0.5f,  0.5f, -0.5f,
-    0.5f,  0.5f, -0.5f,
-    0.5f, -0.5f, -0.5f,
-    -0.5f, -0.5f, 0.5f,
-    -0.5f,  0.5f, 0.5f,
-    0.5f,  0.5f, 0.5f,
-    0.5f, -0.5f, 0.5f,
-    -0.5f, -0.5f, -0.5f,
-    -0.5f, -0.5f,  0.5f,
-    -0.5f,  0.5f,  0.5f,
-    -0.5f,  0.5f, -0.5f,
-    0.5f, -0.5f, -0.5f,
-    0.5f, -0.5f,  0.5f,
-    0.5f,  0.5f,  0.5f,
-    0.5f,  0.5f, -0.5f,
-};
-
-float cubeNormals[] =
-{
-    0.0f, -1.0f, 0.0f,
-    0.0f, -1.0f, 0.0f,
-    0.0f, -1.0f, 0.0f,
-    0.0f, -1.0f, 0.0f,
-    0.0f, 1.0f, 0.0f,
-    0.0f, 1.0f, 0.0f,
-    0.0f, 1.0f, 0.0f,
-    0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, -1.0f,
-    0.0f, 0.0f, -1.0f,
-    0.0f, 0.0f, -1.0f,
-    0.0f, 0.0f, -1.0f,
-    0.0f, 0.0f, 1.0f,
-    0.0f, 0.0f, 1.0f,
-    0.0f, 0.0f, 1.0f,
-    0.0f, 0.0f, 1.0f,
-    -1.0f, 0.0f, 0.0f,
-    -1.0f, 0.0f, 0.0f,
-    -1.0f, 0.0f, 0.0f,
-    -1.0f, 0.0f, 0.0f,
-    1.0f, 0.0f, 0.0f,
-    1.0f, 0.0f, 0.0f,
-    1.0f, 0.0f, 0.0f,
-    1.0f, 0.0f, 0.0f,
-};
-
-float cubeTex[] =
-{
-    0.0f, 0.0f,
-    0.0f, 1.0f,
-    1.0f, 1.0f,
-    1.0f, 0.0f,
-    1.0f, 0.0f,
-    1.0f, 1.0f,
-    0.0f, 1.0f,
-    0.0f, 0.0f,
-    0.0f, 0.0f,
-    0.0f, 1.0f,
-    1.0f, 1.0f,
-    1.0f, 0.0f,
-    0.0f, 0.0f,
-    0.0f, 1.0f,
-    1.0f, 1.0f,
-    1.0f, 0.0f,
-    0.0f, 0.0f,
-    0.0f, 1.0f,
-    1.0f, 1.0f,
-    1.0f, 0.0f,
-    0.0f, 0.0f,
-    0.0f, 1.0f,
-    1.0f, 1.0f,
-    1.0f, 0.0f,
-};
-
-// Allocate memory for buffers
-if ( vertices != NULL )
-{
-    *vertices = (float *)malloc ( sizeof ( float ) * 3 * numVertices );
-    memcpy ( *vertices, cubeVerts, sizeof ( cubeVerts ) );
-    
-    for ( i = 0; i < numVertices * 3; i++ )
-    {
-        ( *vertices ) [i] *= scale;
-    }
-}
-
-if ( normals != NULL )
-{
-    *normals = (float *)malloc ( sizeof ( float ) * 3 * numVertices );
-    memcpy ( *normals, cubeNormals, sizeof ( cubeNormals ) );
-}
-
-if ( texCoords != NULL )
-{
-    *texCoords = (float *)malloc ( sizeof ( float ) * 2 * numVertices );
-    memcpy ( *texCoords, cubeTex, sizeof ( cubeTex ) ) ;
-}
-
-
-// Generate the indices
-if ( indices != NULL )
-{
-    GLuint cubeIndices[] =
-    {
-        0, 2, 1,
-        0, 3, 2,
-        4, 5, 6,
-        4, 6, 7,
-        8, 9, 10,
-        8, 10, 11,
-        12, 15, 14,
-        12, 14, 13,
-        16, 17, 18,
-        16, 18, 19,
-        20, 23, 22,
-        20, 22, 21
-    };
-    
-    *indices = (int *)malloc ( sizeof ( int ) * numIndices );
-    memcpy ( *indices, cubeIndices, sizeof ( cubeIndices ) );
-}
-
-return numIndices;
-}
-
+// Generate Canvas for GLESText - the letters will be drawn on this as a texture
 int GLESRenderer::GenTextCanvas(GLfloat **vertices) {
     
     // Allocate memory for buffers
-    if ( vertices != NULL )
-    {
+    if (vertices != NULL) {
         *vertices = (GLfloat *)malloc(sizeof(GLfloat) * 5 * textCanvasNumVert);
         memcpy(*vertices, textCanvasData, sizeof(textCanvasData));
     }
